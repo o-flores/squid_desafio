cf7bc0f5c3ee2adccd774ed4eb0ef36c9ec70221
Orlando Flores <orlando_flores@id.uff.br>
2021-09-16 19:05:11 -0300

    feat: creates loading state and conditional renderization for loading message

--- src/components/PostsContainer/index.jsx
+++ src/components/PostsContainer/index.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import PostCard from '../PostCard';
+import './style.css';
 
 function PostsContainer() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
+      setLoading(true);
       try {
         const response = await fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic');
         const data = await response.json();
@@ -18,25 +21,29 @@ function PostsContainer() {
       }
     }
     fetchPosts();
+    setLoading(false);
   }, []);
 
   console.log(posts[9]);
   return (
     <div>
-      {error && 'Houve um erro, por favor tente novamente mais tarde'}
-      {posts.length > 0 && posts.map(({
-        uid, legenda, imagens, usuario: { username }, comentarios, upvotes, criadoEm,
-      }) => (
-        <PostCard
-          key={uid}
-          legenda={legenda}
-          imagens={imagens}
-          username={username}
-          comentarios={comentarios}
-          upvotes={upvotes}
-          criadoEm={criadoEm}
-        />
-      ))}
+      {!loading && error && 'Houve um erro, por favor tente novamente mais tarde'}
+      {!error && loading && 'loading'}
+      <div className="post-container">
+        {posts.length > 0 && posts.map(({
+          uid, legenda, imagens, usuario: { username }, comentarios, upvotes, criadoEm,
+        }) => (
+          <PostCard
+            key={uid}
+            legenda={legenda}
+            imagens={imagens}
+            username={username}
+            comentarios={comentarios}
+            upvotes={upvotes}
+            criadoEm={criadoEm}
+          />
+        ))}
+      </div>
     </div>
   );
 }
