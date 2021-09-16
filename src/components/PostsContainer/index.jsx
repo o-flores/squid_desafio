b93abda082262e776af62115195638186d2850ef
Orlando Flores <orlando_flores@id.uff.br>
2021-09-16 12:38:39 -0300

    feat: fetch mockPost info and set a new state

--- src/components/PostsContainer/index.jsx
+++ src/components/PostsContainer/index.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import MockPosts from './post';
 
 function PostsContainer() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // async function fetchPosts() {
+    //   try {
+    //     const response = await fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic', { method:  'GET', redirect: 'follow' });
+    //     console.log(response);
+    //     const data = await response.json();
+    //     setPosts(data);
+    //   } catch (e) {
+    //     // console.log(e);
+    //   }
+    // }
+    // fetchPosts();
+    setPosts(MockPosts)
+  }, []);
+
   return (
-    <div>oi</div>
+    <div>hello world</div>
   );
 }
 
