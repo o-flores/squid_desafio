de8fe4752475bd93803bf627787ae733cf5717a4
Orlando Flores <orlando_flores@id.uff.br>
2021-09-16 15:56:59 -0300

    feat: implements fetchPosts func

--- src/components/PostsContainer/index.jsx
+++ src/components/PostsContainer/index.jsx
@@ -1,27 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import PostCard from '../PostCard';
-import MockPosts from './post';
 
 function PostsContainer() {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    // async function fetchPosts() {
-    //   try {
-    //     const response = await fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic', { method:  'GET', redirect: 'follow' });
-    //     console.log(response);
-    //     const data = await response.json();
-    //     setPosts(data);
-    //   } catch (e) {
-    //     // console.log(e);
-    //   }
-    // }
-    // fetchPosts();
-    setPosts(MockPosts);
+    async function fetchPosts() {
+      try {
+        const response = await fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic');
+        const data = await response.json();
+        setPosts(data);
+        setError(false);
+      } catch (e) {
+        console.log(e);
+        setError(true);
+      }
+    }
+    fetchPosts();
   }, []);
   console.log(posts[0]);
   return (
     <div>
+      {error && 'Houve um erro, por favor tente novamente mais tarde'}
       {posts.length > 0 && posts.map(({ uid, legenda, imagens }) => (
         <PostCard
           key={uid}
