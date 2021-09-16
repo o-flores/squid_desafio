6e679787611f26482f417961b5bf0b756a5a0953
Orlando Flores <orlando_flores@id.uff.br>
2021-09-16 13:06:13 -0300

    feat: add Proptypes

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function PostCard({imagens, legenda}) {
+function PostCard({ imagens, legenda }) {
   return (
     <img
       src={imagens.thumbnail.url}
       alt={legenda}
-      style={{width: '150px', height: '150px'}}
+      style={{ width: '150px', height: '150px' }}
     />
   );
 }
+
+const imagesType = PropTypes.shape({
+  height: PropTypes.number,
+  width: PropTypes.number,
+  url: PropTypes.string,
+});
+
+PostCard.propTypes = {
+  imagens: PropTypes.shape({
+    resolucaoMedia: imagesType,
+    resolucaoPadrao: imagesType,
+    thumbnail: imagesType,
+  }),
+  legenda: PropTypes.string,
+}.isRequired;
 
 export default PostCard;
