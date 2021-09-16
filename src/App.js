618ac80b48f42020d6feb8d5799847d633626a45
Orlando Flores <orlando_flores@id.uff.br>
2021-09-16 09:51:02 -0300

    feat: add squid logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      hello world
+    <div>
+      <img src={Logo} alt="Squid Logo" />
     </div>
   );
 }
